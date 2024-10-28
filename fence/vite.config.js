81c32517ae703a8c6b036738c4ba90628022b9be
satyam <satyamgupta55503@gmail.com>
2024-10-28 11:07:16 +0530

    commit

--- fence/vite.config.js
+++ fence/vite.config.js
@@ -1,11 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   port: 4000,
-// })
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  port: 4000,
+})
 
 
 
@@ -21,13 +21,13 @@
 
 
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0', // Sabhi network interfaces ke liye
-        port: 4000, // Yahan port number bhi change karein agar zaroorat ho
-    },
-});
+// export default defineConfig({
+//     plugins: [react()],
+//     server: {
+//         host: '0.0.0.0', 
+//         port: 4000,
+//     },
+// });
